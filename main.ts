9a20dd789dbaf82740e1cbec765de18e940e5a20
Josiah Hester <josiah.hester@gmail.com>
2021-01-29 10:04:25 -0600

    One more update and test for block types

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
-    fram.write8(0, 11)
+    fram.write8(1, 12)
     basic.pause(100)
-    serial.writeLine("FRAM at 0x0: " + ("" + fram.read8(0)))
+    serial.writeLine("FRAM at 0x0: " + ("" + fram.read8(1)))
 })
 fram.begin()
